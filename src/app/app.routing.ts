55854df7308d5fb1d6a29de034ad9c4a6a705206
lax-seanja <Sean.Jaw@expeditors.com>
2024-07-15 16:03:16 -0700

    added routing to home, coffee list, and coffee form

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -1,13 +1,19 @@
 import { Ng2StateDeclaration, Transition } from '@uirouter/angular';
 import { HomeComponent } from './home/home.component';
 import { CoffeeFormComponent } from './coffee-form/coffee-form.component';
+import { CoffeeListComponent } from './coffee-list/coffee-list.component';
 
 export const appRouting: Ng2StateDeclaration[] = [
 	{
-		name: 'Home',
+		name: 'home',
 		url: '/home',
 		component: HomeComponent,
 	},
-	{ name: 'coffee-form', url: '/coffee', component: CoffeeFormComponent },
+	{ name: 'coffeeForm', url: '/coffee', component: CoffeeFormComponent },
 	// TODO: Add Routes to list coffees, add coffee and edit/delete coffee
+	{
+		name: 'coffeeList',
+		url: '/coffee-list',
+		component: CoffeeListComponent,
+	},
 ];
