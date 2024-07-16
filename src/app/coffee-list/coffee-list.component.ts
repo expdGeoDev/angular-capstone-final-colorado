638ba72fd22217f3effae110595fc0aa15623bd6
lax-seanja <Sean.Jaw@expeditors.com>
2024-07-16 12:40:32 -0700

    added sorting to coffee list

--- src/app/coffee-list/coffee-list.component.ts
+++ src/app/coffee-list/coffee-list.component.ts
@@ -1,31 +1,65 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import {CoffeeModel} from "../model/CoffeeModel";
 import {HttpService} from "../service/http.service";
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-coffee-list',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './coffee-list.component.html',
   styleUrl: './coffee-list.component.css'
 })
-export class CoffeeListComponent {
+export class CoffeeListComponent implements OnInit{
+
 
 
 	list: CoffeeModel[] = [];
+	sortedData: CoffeeModel[] =[];
 	sortOrder = 'asc';
-	sortKey = '';
-
+	sortKey: keyof CoffeeModel = 'id';
 	constructor( private coffeeService: HttpService) {
-
 	}
 
-	async ngOnInit() {
+	ngOnInit() {
 
-		await this.coffeeService.getAllCoffee().subscribe( coffeList=> this.list=coffeList );
+		  this.coffeeService.getAllCoffee().subscribe( coffeList=> {this.list=coffeList;
+			this.sortedData = [...this.list];
+			});
 
 	}
+	sortTable(key: keyof CoffeeModel) {
+		this.sortKey = key;
+		this.sortOrder = this.sortOrder === 'asc' ? 'desc' : 'asc';
 
+		this.sortedData = this.list.sort((a, b) => {
+			const aValue = key === 'id' ? +a[key] : a[key];
+			const bValue = key === 'id' ? +b[key] : b[key];
+
+			if (aValue === undefined || bValue === undefined) {
+				return 0; // Treat undefined values as equal
+			}
+
+			// if (this.sortedKey == 'id'){
+			//
+			// }
+
+			if (aValue < bValue) {
+				return this.sortOrder === 'asc' ? -1 : 1;
+			} else if (aValue > bValue) {
+				return this.sortOrder === 'asc' ? 1 : -1;
+			} else {
+				return 0;
+			}
+		});
+	}
+
+	getSortClass(key: keyof CoffeeModel): string {
+		if (this.sortKey !== key) {
+			return '';
+		}
+		return this.sortOrder === 'asc' ? 'bi bi-caret-up-fill' : 'bi bi-caret-down-fill';
+	}
 
 
 }
