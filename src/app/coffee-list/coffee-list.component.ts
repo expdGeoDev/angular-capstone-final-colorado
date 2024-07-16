fea2c3698563c228db6369ea644c0641c879c5b2
Komal Patel <komal.patelexpeditors.com>
2024-07-16 12:37:07 -0400

    coffee-list branch

--- src/app/coffee-list/coffee-list.component.ts
+++ src/app/coffee-list/coffee-list.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {CoffeeModel} from "../model/CoffeeModel";
+import {HttpService} from "../service/http.service";
 
 @Component({
   selector: 'app-coffee-list',
@@ -8,5 +10,22 @@ import { Component } from '@angular/core';
   styleUrl: './coffee-list.component.css'
 })
 export class CoffeeListComponent {
+
+
+	list: CoffeeModel[] = [];
+	sortOrder = 'asc';
+	sortKey = '';
+
+	constructor( private coffeeService: HttpService) {
+
+	}
+
+	async ngOnInit() {
+
+		await this.coffeeService.getAllCoffee().subscribe( coffeList=> this.list=coffeList );
+
+	}
+
+
 
 }
