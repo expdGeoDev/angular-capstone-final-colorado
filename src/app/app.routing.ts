0045018c6e673caebeb36ed07807e09dd70d666e
Komal Patel <komal.patelexpeditors.com>
2024-07-17 11:24:24 -0400

    coffee-edit-delete

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -2,18 +2,25 @@ import { Ng2StateDeclaration, Transition } from '@uirouter/angular';
 import { HomeComponent } from './home/home.component';
 import { CoffeeFormComponent } from './coffee-form/coffee-form.component';
 import { CoffeeListComponent } from './coffee-list/coffee-list.component';
-
+import { CoffeeEditDelete } from './coffee-edit-delete/coffee-edit-delete.component'
 export const appRouting: Ng2StateDeclaration[] = [
 	{
 		name: 'home',
 		url: '/home',
 		component: HomeComponent,
 	},
-	{ name: 'coffeeForm', url: '/coffee', component: CoffeeFormComponent },
+	{ name: 'coffeeForm',
+		url: '/coffee',
+		component: CoffeeFormComponent
+	},
 	// TODO: Add Routes to list coffees, add coffee and edit/delete coffee
 	{
 		name: 'coffeeList',
 		url: '/coffee-list',
 		component: CoffeeListComponent,
+	},
+	{ name: 'coffeeEditDelete',
+		url: '/edit-delete-coffee',
+		component: CoffeeEditDelete
 	},
 ];
---
+++ src/app/coffee-edit-delete/coffee-edit-delete.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import {HttpService} from "../service/http.service";
+
+@Component({
+  selector: 'app-coffee-edit-delete',
+  standalone: true,
+  imports: [],
+  templateUrl: './coffee-edit-delete.component.html',
+  styleUrl: './coffee-edit-delete.component.css'
+})
+
+export class CoffeeEditDelete {
+}
