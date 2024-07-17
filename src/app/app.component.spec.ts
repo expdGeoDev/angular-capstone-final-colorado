b165445a86ce367a3d61ecf4f66838ab4c8bb17f
Edwin Soto <edwin.soto03@gmail.com>
2024-07-17 15:30:47 -0400

    Created tests for http service.

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -1,11 +1,21 @@
-import { TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { AppComponent } from './app.component';
 
 describe('AppComponent', () => {
-  beforeEach(async () => {
+
+	let component: AppComponent;
+	let fixture: ComponentFixture<AppComponent>;
+
+	beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [AppComponent],
     }).compileComponents();
+		fixture = TestBed.createComponent(AppComponent);
+		component = fixture.componentInstance;
+		fixture.detectChanges();
   });
 
+	it('should create the app', () => {
+		expect(component).toBeTruthy();
+	});
 });
---
+++ src/app/mocks/mockCoffee.ts
@@ -0,0 +1,77 @@
+import { CoffeeModel, RoastType, SizeType } from '../model/CoffeeModel';
+
+const mockCoffee1: CoffeeModel = {
+	"id": 1,
+	"active": false,
+	"roaster": "Tim Horton's",
+	"variety": '',
+	"size": 14,
+	"roast": RoastType.DARK,
+	"format": "k-pod",
+	"grind": SizeType.EIGHT,
+	"origin": [''],
+	"singleOrigin": true,
+	"tastingNotes": ''
+};
+const mockCoffee2: CoffeeModel =
+{
+	"id": 2,
+	"active": false,
+	"roaster": "Tim Horton's",
+	"variety": '',
+	"size": SizeType.TWENTY,
+	"roast": RoastType.DARK,
+	"format": "k-pod",
+	"grind": 4,
+	"origin": [''],
+	"singleOrigin": false,
+	"tastingNotes": ''
+};
+const mockCoffee3: CoffeeModel =
+{
+	"id": 3,
+	"active": false,
+	"roaster": "Paper Plane",
+	"variety": '',
+	"size": SizeType.EIGHT,
+	"roast": RoastType.MEDIUM,
+	"format": "bean",
+	"grind": 6,
+	"origin": [''],
+	"singleOrigin": false,
+	"tastingNotes": ''
+};
+const mockCoffee4: CoffeeModel =
+{
+	"id": 4,
+	"active": false,
+	"roaster": "Paper Plane",
+	"variety": '',
+	"size": SizeType.FOURTEEN,
+	"roast": RoastType.MEDIUM,
+	"format": "ground",
+	"grind": 9,
+	"origin": [''],
+	"singleOrigin": true,
+	"tastingNotes": ''
+};
+const mockCoffee5: CoffeeModel =
+{
+	"id": 5,
+	"active": false,
+	"roaster": "Eight O'Clock",
+	"variety": '',
+	"size": SizeType.TWENTYFOUR,
+	"roast": RoastType.DARK,
+	"format": "ground",
+	"grind": 7,
+	"origin": [''],
+	"singleOrigin": false,
+	"tastingNotes": ''
+};
+
+const mockCoffeeArray: CoffeeModel[] = [mockCoffee1, mockCoffee2, mockCoffee3, mockCoffee4, mockCoffee5]
+
+const mockCoffeeArrayByRoaster: CoffeeModel[] = [mockCoffee3, mockCoffee4];
+
+export { mockCoffee1, mockCoffee2, mockCoffee3, mockCoffee4, mockCoffeeArray, mockCoffeeArrayByRoaster };
