2db85b46084fa9e816e6fbf4f897573cb402d179
Antony Alfaro <antony.alfaro@expeditors.com>
2024-07-18 12:21:49 -0600

    Add tests to coffee list component

--- src/app/coffee-list/coffee-list.component.spec.ts
+++ src/app/coffee-list/coffee-list.component.spec.ts
@@ -1,24 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CoffeeListComponent } from './coffee-list.component';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpService } from '../service/http.service';
+import { jsDocComment } from '@angular/compiler';
+import { of } from 'rxjs';
+import { mockCoffeeArray } from '../mocks/mockCoffee';
 
 describe('CoffeeListComponent', () => {
-  let component: CoffeeListComponent;
-  let fixture: ComponentFixture<CoffeeListComponent>;
+	let component: CoffeeListComponent;
+	let mockHttpService: any;
+	let mockSpinnerService;
+	let mockModalService;
 
-  beforeEach(async () => {
-    await TestBed.configureTestingModule({
-      imports: [HttpClientModule, CoffeeListComponent]
-    })
-    .compileComponents();
+	beforeEach(() => {
+		mockHttpService = jasmine.createSpyObj(['getAllCoffee']);
+		mockSpinnerService = jasmine.createSpyObj(['show', 'hide']);
+		mockModalService = jasmine.createSpyObj(['close', 'open']);
 
-    fixture = TestBed.createComponent(CoffeeListComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+		component = new CoffeeListComponent(mockHttpService, mockSpinnerService, mockModalService);
+	});
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+	it('should create', () => {
+		expect(component).toBeTruthy();
+	});
+
+	it('should get all coffees', () => {
+		mockHttpService.getAllCoffee.and.returnValue(of(mockCoffeeArray));
+
+		component.list = mockCoffeeArray;
+
+		expect(component.list.length).toEqual(5);
+	});
 });
