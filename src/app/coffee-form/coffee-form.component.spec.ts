0e90d33ae5bc13650afef46daed8e08a6c3a6a5b
Edwin Soto <edwin.soto03@gmail.com>
2024-07-19 10:30:19 -0400

    initial testing for coffe form.

--- src/app/coffee-form/coffee-form.component.spec.ts
+++ src/app/coffee-form/coffee-form.component.spec.ts
@@ -38,6 +38,14 @@ describe('CoffeeFormComponent', () => {
 		expect(component.coffeeForm.get('grind')?.value).toBe(0);
 	});
 
+	// TODO: How Do we parameterize test
+	it('should return a grid value', () => {
+		let strValue = 'Extra Fine';
+
+		strValue = component.getGrindLevel(10);
+		expect(strValue).toBe('Extra Fine');
+	});
+
 	it('should have roaster', () => {
 		const roaster = component.coffeeForm.get('roaster');
 		expect(roaster?.valid).toBeFalsy();
